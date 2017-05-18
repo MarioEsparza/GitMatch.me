25a5b66a46f11d4b49961c51d17c0b40d3a2990d
Mario Esparza <marioesparza818@gmail.com>
2017-05-18 09:43:30 -0700

    Match Me Update

--- js/services/locationService.js
+++ js/services/locationService.js
@@ -1,8 +1,10 @@
 app.factory('locationService', ['$http', function ($http) {
     return {
         getUsers: function (location, language) {
-        console.log("Service Location: ", location, "FilterBy: ", language)
-        return $http.get('https://api.github.com/search/users?q=location%3A' + location + language)
+        //console.log("Service Location: ", location, "FilterBy: ", language)
+        var APIurl = 'https://api.github.com/search/users?q=location%3A' + location + language;
+        console.log("Location Service: ", APIurl)
+        return $http.get(APIurl)
                       .then(function (result) {
                           return result.data;
                       });
