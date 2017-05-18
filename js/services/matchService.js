3b39cb3cdaf6b3bf2fb9ea8884a50f86c7def8f5
Mario Esparza <marioesparza818@gmail.com>
2017-05-18 09:44:15 -0700

    Match Me Update

---
+++ js/services/matchService.js
@@ -0,0 +1,20 @@
+﻿app.factory('matchService', ['$http', '$q', function ($http, $q) {
+
+    var matchService = function () {
+    };
+        matchService.getLocation = function (username) {
+               return $http.get('https://api.github.com/users/' + username)
+                       .then(function (result) {
+                           return result.data;
+                       });
+         };
+
+        matchService.getRepos = function (username) {
+               return $http.get('https://api.github.com/users/' + username + '/repos')
+                             .then(function (result) {
+                                 return result.data;
+                             });
+    };
+        return matchService;
+
+}]);
