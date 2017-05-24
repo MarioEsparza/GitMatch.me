3c1577874bf312a081e81454580159be0fa50a40
Brett Reinhard <brettreinhard@gmail.com>
2017-05-24 13:54:41 -0700

    Update locationService.js

--- js/services/locationService.js
+++ js/services/locationService.js
@@ -1,10 +1,21 @@
 app.factory('locationService', ['$http', function ($http) {
     return {
         getUsers: function (location, language) {
-        //console.log("Service Location: ", location, "FilterBy: ", language)
-        var APIurl = 'https://api.github.com/search/users?q=location%3A' + location + language;
-        console.log("Location Service: ", APIurl)
-        return $http.get(APIurl)
+            //console.log("Service Location: ", location, "FilterBy: ", language)
+            var options = {
+
+                method: 'GET',
+                url: 'https://api.github.com/search/users?q=location%3A' + location + language,
+                headers:
+                    {
+                        'Authorization': "token 136c84eafb2c6070f054884a337f48884dc48aad"
+                    }
+
+
+            }
+        
+        
+        return $http(options)
                       .then(function (result) {
                           return result.data;
                       });
