1a810d094f32e8d7bb4d27a834036a36947e0e32
Brett Reinhard <brettreinhard@gmail.com>
2017-05-24 13:54:18 -0700

    Update matchService.js

--- js/services/matchService.js
+++ js/services/matchService.js
@@ -2,17 +2,43 @@ app.factory('matchService', ['$http', '$q', function ($http, $q) {
 
     var matchService = function () {
     };
-        matchService.getLocation = function (username) {
-               return $http.get('https://api.github.com/users/' + username)
+    matchService.getLocation = function (username) {
+        var options = {
+            
+                method: 'GET',
+            url: 'https://api.github.com/users/' + username,
+            headers: 
+                {
+                    'Authorization': "token 136c84eafb2c6070f054884a337f48884dc48aad"
+                }
+
+
+        }
+
+
+        
+               return $http(options)
                        .then(function (result) {
                            return result.data;
                        });
          };
 
-        matchService.getRepos = function (username,j) {
-               return $http.get('https://api.github.com/users/' + username + '/repos?sort=updated')
+    matchService.getRepos = function (username, j) {
+        var options = {
+
+            method: 'GET',
+            url: 'https://api.github.com/users/' + username + '/repos?sort=updated',
+            headers:
+                {
+                    'Authorization': "token 136c84eafb2c6070f054884a337f48884dc48aad"
+                }
+
+
+        }
+               return $http(options)
                              .then(function (result) {
-                                 console.log(result);
+                                 console.log(result.data);
+                                 
                                  result.data.newIndex = j;
                                  return result.data;
                              });
